9a05c773487b3aff6ce6253dbd3273a33d765a75
Ron Nair <r.nair@pokemon.com>
2019-10-12 11:53:07 -0700

    Adding bookapp example and wsg1 simple ex

--- wsgi/bookapp.py
+++ wsgi/bookapp.py
@@ -6,18 +6,62 @@ DB = BookDB()
 
 
 def book(book_id):
-    return "<h1>a book with id %s</h1>" % book_id
+    book_info = DB.title_info(book_id)
+    if book_info:
+        body = [f"<h1>{book_info['title']}</h1>",
+                "<ul>",
+                f"<li>{book_info['isbn']}</li>",
+                f"<li>{book_info['publisher']}</li>",
+                f"<li>{book_info['author']}</li>",
+                "</ul>"]
+        return "\n".join(body)
+    raise NameError
 
 
 def books():
-    return "<h1>a list of books</h1>"
+    title_rows = DB.titles()
+    body = ["<h1>Book List</h1>", "<ul>"]
+    for row in title_rows:
+        body.append(f"<li><a href=\"book/{row['id']}\">{row['title']}</a></li>")
+    body.append("</ul>")
+    return "\n".join(body)
+
+
+def resolve_path(path):
+    functions = {
+        '': books,
+        'book': book
+    }
+
+    path = path.strip('/').split('/')
+    try:
+        function = functions[path[0]]
+        arguments = path[1:]
+        return function, arguments
+    except IndexError:
+        raise NameError
 
 
 def application(environ, start_response):
-    status = "200 OK"
     headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    return ["<h1>No Progress Yet</h1>".encode('utf8')]
+    path = environ.get('PATH_INFO', None)
+    try:
+        if not path:
+            raise NameError
+        function, args = resolve_path(path)
+        body = function(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception as exception:
+        status = "500 Internal Server Error"
+        body = "<h1> Internal Server Error</h1>"
+        print(str(exception))
+    finally:
+        headers.append(('Content-Length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 
 if __name__ == '__main__':
--- wsgi/wsgi_1.py
+++ wsgi/wsgi_1.py
@@ -18,14 +18,14 @@ body = """<html>
 def application(environ, start_response):
     import pprint
     pprint.pprint(environ)
-
+    date = datetime.datetime.now()
     response_body = body.format(
         software=environ.get('SERVER_SOFTWARE', default),
-        path="aaaa",
-        month="bbbb",
-        date="cccc",
-        year="dddd",
-        client_ip="eeee"
+        path=environ.get('PATH_INFO', default),
+        month=date.strftime('%B'),
+        date=date.day,
+        year=date.year,
+        client_ip=environ.get('REMOTE_ADDR', default)
     )
     status = '200 OK'
 
